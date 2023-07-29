0f3c367c0a0c0cbc3cc7bd766296125ca42c962a
psw1305 <shshck5@gmail.com>
2023-07-29 16:20:30 +0900

    23.07.29 아이템 추가
    유물 아이템 15종 추가

--- UnityProject-PPR/Assets/_ASSETS_/Scripts/Game/GameManager.cs
+++ UnityProject-PPR/Assets/_ASSETS_/Scripts/Game/GameManager.cs
@@ -1,14 +1,10 @@
 using UnityEngine;
-using TMPro;
 
 public class GameManager : BehaviourSingleton<GameManager>
 {
+    [Header("Camera")]
     [SerializeField] private Camera mainCamera;
     public Camera MainCamera => this.mainCamera;
-
-    // 프레임 테스트용 텍스트
-    public TextMeshProUGUI fpsText;
-    private float deltaTime = 0;
 
     protected override void Awake()
     {
@@ -16,18 +12,6 @@ public class GameManager : BehaviourSingleton<GameManager>
 
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = DataFrame.Instance.dFPS;
-    }
-
-    /// <summary>
-    /// 향시 프레임 확인
-    /// </summary>
-    void Update()
-    {
-        if (fpsText == null) return;
-
-        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
-        float fps = 1.0f / deltaTime;
-        fpsText.text = Mathf.Ceil(fps).ToString();
     }
 
     public void CameraChange(Canvas canvas)
